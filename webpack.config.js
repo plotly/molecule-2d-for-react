c9e6e4aaba5d294d56a8627b17c7e112de9ebec9
Hammad <hammadtheone@gmail.com>
2024-02-12 00:25:39 -0500

    Update webpack and rebuild package

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
       }, {
         test: /\.scss$/,
         use: [
